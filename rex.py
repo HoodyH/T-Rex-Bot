7e6cb573f99b16ad9bd5cd880b1eae40c403c1a5
simone <not.simone96@gmail.com>
2019-12-09 17:00:51 +0100

    smart jump version

--- rex.py
+++ rex.py
@@ -5,16 +5,36 @@ from mss.linux import MSS as mss
 import mss.tools
 from numpy import array, mean
 
-monitor = {'top': 414, 'left': 900, 'width': 70, 'height': 35}
+"""
+Set this data
+"""
+screen_resolution_w = 1920
+screen_resolution_h = 1080
+
+top = 415
+left = 830
+
+
+"""
+top = 414
+left = 900
+
+You can play here http://www.trex-game.skipser.com/
+Calibrate the tail of the t-rex at the start of the calibration monitor 
+"""
+
+calibration_monitor = {'top': top, 'left': left, 'width': 200, 'height': 35}
+pterodactylus_monitor = {'top': top + 30, 'left': left+100, 'width': 70, 'height': 5}
+kaktus_monitor = {'top': top, 'left': left+90, 'width': 80, 'height': 35}
 
 
 def up():
+    print('jump')
     pg.press("space")
-    # time.sleep(0.8)
-    # pg.press("down")
 
 
 def down():
+    print('crow')
     pg.press("down")
 
 
@@ -29,42 +49,55 @@ def process_image(original_image):
 def boot():
 
     sct = mss.mss()
+    jumped = False
 
     while True:
         # Creates a full snapshot of the screen and returns an RGB image
-        sct_img = sct.grab(monitor)
+        sct_kaktus = sct.grab(kaktus_monitor)
+        sct_pterodactylus = sct.grab(pterodactylus_monitor)
 
-        sct_img = array(sct_img)
-        processed_image = process_image(sct_img)
+        sct_kaktus = array(sct_kaktus)
+        processed_image = process_image(sct_kaktus)
 
         gradient = mean(processed_image)
-        print('gradient = ', gradient)
 
         if int(gradient) is not 0:
             up()
+            jumped = True
 
-        time.sleep(0.095)
+        if int(gradient) is 0 and jumped is True:
+            down()
+            jumped = False
+
+        time.sleep(0.080)
 
 
 def snapshot():
 
     with mss.mss() as sct:
-        # The screen part to capture
-        output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
-
-        # Grab the data
-        sct_img = sct.grab(monitor)
-
+        output = "sct-{top}x{left}.png".format(**calibration_monitor)
+        # grub the image
+        sct_img = sct.grab(calibration_monitor)
         # Save to the picture file
         mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+
+    print('snapshot done look in root folder')
 
 
 if __name__ == "__main__":
     print('Before run the bot calibrate the window with a snapshot')
-    choice = input('1) start the BOT\n2) calibrate with a SNAPSHOT\n')
-    print(choice)
-    if choice == '1':
-        boot()
+    print('1 Start the BOT')
+    print('2 Calibrate with a SNAPSHOT')
+    print('0 or ctr-C to exit')
 
-    if choice == '2':
-        snapshot()
+    while True:
+        choice = int(input())
+
+        if choice is 0:
+            break
+
+        if choice is 1:
+            boot()
+
+        if choice is 2:
+            snapshot()
